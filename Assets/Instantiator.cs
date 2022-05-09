9bf93232f981c44d8d6e67fc179294746e5201de
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-09 10:49:57 -0300

    Initial commit

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectToClone;        
+
+    public void CloneObject()
+    {
+
+        Instantiate(objectToClone);
+
+    }
+}
---
+++ Assets/ShowMessageInConsole.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMessageInConsole : MonoBehaviour
+{
+    public void ShowMessage()
+    {
+        Debug.Log("Hello world!!!");
+    }
+}
